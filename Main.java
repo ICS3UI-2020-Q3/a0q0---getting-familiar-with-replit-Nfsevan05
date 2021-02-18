121e978d4ef5acc3864a2a94e636187d5a05a7eb
Nfsevan05 <schee3492@gmail.com>
2021-02-18 16:31:04 +0000

    Age calculation code

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * This is the hello world program
  * Evan Schedewitz
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-   System.out.println("hello world");
-    
+   //Ask user for their name
+   System.out.println("Please enter your name:");
+   // creating a scanner for user input
+    Scanner input = new Scanner(System.in);
+    // Declare and initalize the name varible 
+    String name = input.nextLine();
+    //say hello to the user
+    System.out.println("hello " + name);
+    //ask the user when they were born
+    System.out.println("Please enter the year you were born:");
+    //create the varible to store the birth year
+    int birthYear = input.nextInt();
+    //caclulate the approximate age
+    int age = 2021 - birthYear;
+    //tell user calculated age
+    System.out.println("You are approximately " + age + " years old.");
   }
 }
